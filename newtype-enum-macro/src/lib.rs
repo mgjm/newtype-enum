c8eda405946c2fdff5610e45c4d9b9d144492b5c
Martin Michaelis <code@mgjm.de>
2023-04-08 08:19:07 +0000

    Update dependencies

--- newtype-enum-macro/src/lib.rs
+++ newtype-enum-macro/src/lib.rs
@@ -1,15 +1,14 @@
 #![warn(missing_docs, clippy::all, clippy::pedantic, clippy::nursery)]
-#![allow(clippy::manual_filter_map, clippy::default_trait_access)]
 
 //! Provide the `#[newtype_enum]` attribute macro to derive the `Enum` and `Variant` traits from the `newtype-enum` crate.
 
 extern crate proc_macro;
 
 use proc_macro2::{Ident, Span, TokenStream};
-use quote::{quote, ToTokens};
+use quote::quote;
 use syn::{
-    parse2, parse_macro_input, parse_quote, parse_str, Error, Fields, ItemEnum, ItemStruct, LitStr,
-    Meta, MetaList, NestedMeta, Path, Variant, VisRestricted, Visibility,
+    meta, parse::Parser, parse_macro_input, parse_quote, token::Struct, Error, Fields, Generics,
+    ItemEnum, ItemStruct, LitStr, Meta, Path, Variant, VisRestricted, Visibility,
 };
 
 /// Derive the `Enum` and `Variant` traits from the `newtype-enum` crate.
@@ -18,9 +17,7 @@ pub fn newtype_enum(
     attr: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    let attr: TokenStream = attr.into();
-    let attr: proc_macro::TokenStream = quote!(_(#attr)).into();
-    newtype_enum_impl(parse_macro_input!(attr), parse_macro_input!(item)).into()
+    newtype_enum_impl(attr.into(), parse_macro_input!(item)).into()
 }
 
 macro_rules! unwrap_or_compile_error {
@@ -32,7 +29,7 @@ macro_rules! unwrap_or_compile_error {
     };
 }
 
-fn newtype_enum_impl(meta: MetaList, item: ItemEnum) -> TokenStream {
+fn newtype_enum_impl(meta: TokenStream, item: ItemEnum) -> TokenStream {
     let e = unwrap_or_compile_error!(NewtypeEnum::new(meta, item));
 
     let enum_item = e.define_enum();
@@ -53,47 +50,45 @@ struct NewtypeEnum {
 }
 
 impl NewtypeEnum {
-    fn new(meta: MetaList, item: ItemEnum) -> Result<Self, Error> {
-        let crate_name = crate_name();
-        let crate_name = parse_quote!(::#crate_name);
+    fn new(meta: TokenStream, item: ItemEnum) -> Result<Self, Error> {
+        let mut crate_name = crate_name();
 
-        let variants = ident_append(&item.ident, "_variants");
-        let variants_vis = item.vis.clone();
-        let mut e = Self {
+        let mut variants = ident_append(&item.ident, "_variants");
+        let mut variants_vis = item.vis.clone();
+
+        if !meta.is_empty() {
+            meta::parser(|meta| {
+                let ident = meta
+                    .path
+                    .get_ident()
+                    .ok_or_else(|| Error::new_spanned(&meta.path, "expected identifier"))?;
+                if ident == "variants" {
+                    let lit: LitStr = meta.value()?.parse()?;
+                    let s = lit.value();
+                    let s = if let Some(index) = s.rfind(' ') {
+                        variants_vis = syn::parse_str(&s[..index])
+                            .map_err(|err| Error::new_spanned(&lit, err))?;
+                        &s[index + 1..]
+                    } else {
+                        &s
+                    };
+                    variants = syn::parse_str(s).map_err(|err| Error::new_spanned(&lit, err))?;
+                } else if ident == "unstable_self_test" {
+                    crate_name = parse_quote!(self);
+                } else {
+                    return Err(Error::new_spanned(ident, "unknown argument"));
+                }
+                Ok(())
+            })
+            .parse2(meta)?;
+        }
+
+        Ok(Self {
             item,
             crate_name,
             variants,
             variants_vis,
-        };
-
-        for meta in meta.nested {
-            match meta {
-                NestedMeta::Meta(Meta::NameValue(meta)) => {
-                    let ident: Ident = parse2(meta.path.to_token_stream())?;
-                    match ident.to_string().as_ref() {
-                        "variants" => {
-                            let lit: LitStr = parse2(meta.lit.to_token_stream())?;
-                            let lit = &lit;
-                            let s = lit.value();
-                            let s = if let Some(index) = s.rfind(' ') {
-                                e.variants_vis = parse_str(&s[..index])
-                                    .map_err(|err| Error::new_spanned(lit, err))?;
-                                &s[index + 1..]
-                            } else {
-                                &s
-                            };
-                            e.variants =
-                                parse_str(s).map_err(|err| Error::new_spanned(lit, err))?;
-                        }
-                        "unstable_self_test" => e.crate_name = parse_quote!(crate),
-                        _ => return Err(Error::new_spanned(ident, "unknown argument")),
-                    }
-                }
-                _ => return Err(Error::new_spanned(meta, "expected a name-value pair")),
-            }
-        }
-
-        Ok(e)
+        })
     }
 
     fn variants(&self) -> impl Iterator<Item = Variant> + '_ {
@@ -122,53 +117,52 @@ impl NewtypeEnum {
     }
 
     fn define_variants(&self) -> TokenStream {
-        let vis = unwrap_or_compile_error!(super_vis(&self.item.vis, || parse_quote!(pub(super))));
-        let items: Vec<_> = self
-            .item
-            .variants
-            .iter()
-            .filter(|var| !matches!(&var.fields, Fields::Unnamed(fields) if fields.unnamed.len() == 1))
-            .map(move |var| {
-                let mut item = ItemStruct {
-                    attrs: self
-                        .item
-                        .attrs
-                        .iter()
-                        .filter(|attr| !matches!(attr.parse_meta(), Ok(Meta::NameValue(meta)) if meta.path.is_ident("doc")))
-                        .chain(var.attrs.iter())
-                        .cloned()
-                        .collect(),
-                    vis: vis.clone(),
-                    struct_token: Default::default(),
-                    ident: var.ident.clone(),
-                    generics: Default::default(),
-                    fields: var.fields.clone(),
-                    semi_token: None,
-                };
-                match &mut item.fields {
-                    Fields::Unit => {
-                        item.semi_token = parse_quote!(;);
-                    }
-                    Fields::Named(fields) => {
-                        for field in &mut fields.named {
-                            field.vis =
-                                unwrap_or_compile_error!(super_vis(&field.vis, || vis.clone()));
-                        }
-                    }
-                    Fields::Unnamed(_) => {
-                        return Error::new_spanned(var, "unsupported variant type")
-                            .to_compile_error();
-                    }
-                };
-                quote!(#item)
-            })
-            .collect();
-        if items.is_empty() {
-            return Default::default();
+        let items = self.item.variants.iter().filter(
+            |var| !matches!(&var.fields, Fields::Unnamed(fields) if fields.unnamed.len() == 1),
+        );
+
+        if items.clone().next().is_none() {
+            return TokenStream::new();
         }
+
+        let vis = unwrap_or_compile_error!(super_vis(&self.item.vis, || parse_quote!(pub(super))));
+        let item_attrs = self.item.attrs.iter().filter(
+            |attr| !matches!(&attr.meta, Meta::NameValue(meta) if meta.path.is_ident("doc")),
+        );
+
+        let items = items.map(move |var| {
+            let mut item = ItemStruct {
+                attrs: item_attrs
+                    .clone()
+                    .chain(var.attrs.iter())
+                    .cloned()
+                    .collect(),
+                vis: vis.clone(),
+                struct_token: Struct::default(),
+                ident: var.ident.clone(),
+                generics: Generics::default(),
+                fields: var.fields.clone(),
+                semi_token: None,
+            };
+            match &mut item.fields {
+                Fields::Unit => {
+                    item.semi_token = parse_quote!(;);
+                }
+                Fields::Named(fields) => {
+                    for field in &mut fields.named {
+                        field.vis = unwrap_or_compile_error!(super_vis(&field.vis, || vis.clone()));
+                    }
+                }
+                Fields::Unnamed(_) => {
+                    return Error::new_spanned(var, "unsupported variant type").to_compile_error();
+                }
+            };
+            quote!(#item)
+        });
+
         let variants = &self.variants;
         let variants_vis = &self.variants_vis;
-        let doc = format!("The generated variants of the `{0}` enum.", self.item.ident);
+        let doc = format!("The generated variants of the `{}` enum.", self.item.ident);
         quote! {
             #[allow(non_snake_case)]
             #[doc = #doc]
@@ -195,9 +189,9 @@ impl NewtypeEnum {
             quote!(
                 #[doc(hidden)]
                 impl #crate_name::unstable::VariantCore<#e> for #ty {
-                     fn into_enum(self) -> #e {
-                         #v(self)
-                     }
+                    fn into_enum(self) -> #e {
+                        #v(self)
+                    }
 
                     fn from_enum(e: #e) -> ::core::option::Option<Self> {
                         #match_from
@@ -211,12 +205,8 @@ impl NewtypeEnum {
                         #match_from
                     }
 
-                    fn is_enum_variant(e: &#e) -> bool
-                    {
-                        match e {
-                            #v(_) => true,
-                            _ => false,
-                        }
+                    fn is_enum_variant(e: &#e) -> bool {
+                        matches!(e, #v(_))
                     }
 
                     fn from_enum_unwrap(e: #e) -> Self {
@@ -253,13 +243,13 @@ fn super_vis(vis: &Visibility, default: impl FnOnce() -> Visibility) -> Result<V
                 if path.leading_colon.is_some() {
                     vis.clone()
                 } else {
-                    parse_quote!( pub(#in_token super::#path))
+                    parse_quote!(pub(#in_token super::#path))
                 }
             } else if let Some(ident) = path.get_ident() {
                 match ident.to_string().as_ref() {
                     "crate" => parse_quote!(pub(crate)),
-                    "self" => parse_quote! (pub(super)),
-                    "super" => parse_quote! (pub(in super::super)),
+                    "self" => parse_quote!(pub(super)),
+                    "super" => parse_quote!(pub(in super::super)),
                     _ => {
                         return Err(Error::new_spanned(ident, "unknown identifier"));
                     }
@@ -268,7 +258,7 @@ fn super_vis(vis: &Visibility, default: impl FnOnce() -> Visibility) -> Result<V
                 return Err(Error::new_spanned(path, "path without `in` token"));
             }
         }
-        vis => vis.clone(),
+        Visibility::Public(v) => Visibility::Public(*v),
     };
     Ok(vis)
 }
@@ -277,8 +267,12 @@ fn ident_append(ident: &Ident, suffix: &str) -> Ident {
     Ident::new(&format!("{ident}{suffix}"), ident.span())
 }
 
-fn crate_name() -> Ident {
-    let crate_name = proc_macro_crate::crate_name("newtype-enum");
-    let crate_name = crate_name.as_ref().map_or("newtype_enum", |n| n);
-    Ident::new(crate_name, Span::call_site())
+fn crate_name() -> Path {
+    match proc_macro_crate::crate_name("newtype-enum") {
+        Ok(proc_macro_crate::FoundCrate::Name(crate_name)) => {
+            let crate_name = Ident::new(&crate_name, Span::call_site());
+            parse_quote!(::#crate_name)
+        }
+        _ => parse_quote!(::newtype_enum),
+    }
 }
